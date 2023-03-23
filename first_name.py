f61abb385c3a11fa1e495b9af608cce45a91cc54
Lovish Arora <lovish.arora484@gmail.com>
2023-03-23 08:23:16 -0500

    Create first_name.py

---
+++ first_name.py
@@ -0,0 +1,8 @@
+# function to print first_name
+def first_name(f_name):
+    print(f'My first name is {f_name}.')
+
+
+if __name__ == '__main__':
+    name = input('Enter your first name: ')
+    first_name(name)
