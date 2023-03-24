df716f128fd8b2ce0b581274a90f54ec136ab2f6
Lovish Arora <lovish.arora484@gmail.com>
2023-03-24 06:21:12 -0500

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,13 @@
+def data_types():
+    e = 10  # integer
+    y = 10.5  # double
+    z = float(10.45)  # float
+    a = True  # True/ False
+    b = 'y'  # character
+    c = 'sample'  # string
+    d = 'sample\'s'  # escape sequence
+    print(d)
+    
+
+if __name__ == '__main__':
+    data_types()
