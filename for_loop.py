da2fe3b6b621fb8e50a51141a196f035060e7597
Lovish Arora <lovish.arora484@gmail.com>
2023-03-24 06:31:41 -0500

    Update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -1,29 +1,57 @@
-# for(int i=0; i<10; i++){ // code here }
+"""
+
+for(int i=0; i<10; i++){
+    // code statements here     0 .. 9
+}
+"""
+
+
 def for_loop():
     for i in range(10):  # i 0..9        0 included 10 excluded
         print(i)
 
 
-# for(int i=3; i< 11; i++){ // code here }   3..10
+"""
+for(int i=3; i<11; i++){
+    // code statements here     3 .. 10
+}
+"""
+
+
 def for_loop_start():
     for i in range(3, 11):  # i 3..10       3 included 11 excluded increment 1
         print(i)
 
 
-# for(int i=3; i< 11; i += 2){ // code here }   3 5 7 9
+"""
+for(int i=3; i<11; i += 2){
+    // code statements here     3 5 7 9
+}
+"""
+
+
 def for_loop_increment():
     for i in range(3, 11, 2):  # i 3 5 7 9       3 included 11 excluded increment 2
         print(i)
 
 
-# for(int i=11; i>3; i -= 2){ // code here }   11 9 7 5
+"""
+for(int i=11; i>3; i -= 2){
+    // code statements here     11 9 7 5
+}
+"""
+
+
 def for_loop_decrement():
     for i in range(11, 3, -2):  # i 11 9 7 5       11 included 3 excluded decrement 2
         print(i)
-        
+
 
 if __name__ == '__main__':
     for_loop()
+    print()
     for_loop_start()
+    print()
     for_loop_increment()
+    print()
     for_loop_decrement()
