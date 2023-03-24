4b7d4669cca882497de082d98b67e4e03a5a92cd
Lovish Arora <lovish.arora484@gmail.com>
2023-03-24 06:32:56 -0500

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -1,5 +1,5 @@
 def if_else():
-    x = int(input('Enter a number:'))
+    x = int(input('Enter a number: '))
     # Boolean expression True/ False
     if x > 1:
         print(f'{x} is greater than 1')
@@ -7,6 +7,7 @@ def if_else():
         print(f'{x} is equal to 1')
     else:
         print(f'{x} is less than 1')
-        
+
+
 if __name__ == '__main__':
     if_else()
