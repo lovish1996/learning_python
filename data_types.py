41c8a8c4371b420f6759cee88e99da788718b9a3
Lovish Arora <lovish.arora484@gmail.com>
2023-03-24 06:28:21 -0500

    Update data_types.py

--- data_types.py
+++ data_types.py
@@ -1,13 +1,19 @@
 def data_types():
     e = 10  # integer
+    print(e)
     y = 10.5  # double
+    print(y)
     z = float(10.45)  # float
+    print(z)
     a = True  # True/ False
+    print(a)
     b = 'y'  # character
+    print(b)
     c = 'sample'  # string
+    print(c)
     d = 'sample\'s'  # escape sequence
     print(d)
-    
+
 
 if __name__ == '__main__':
     data_types()
