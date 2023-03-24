f27dc253aec3c34b4168c5580927b086c29a405f
Lovish Arora <lovish.arora484@gmail.com>
2023-03-24 06:25:20 -0500

    Update while_loop.py

--- while_loop.py
+++ while_loop.py
@@ -1,16 +1,18 @@
-'''
+"""
 int i = 5;
 while(i >= 0){
     cout << i << endl;
     i--;
 }
-'''
+"""
+
+
 def while_loop():
     i = 5
     while i >= 0:
         print(i)  # 5 4 3 2 1 0
         i -= 1
-        
-        
+
+
 if __name__ == '__main__':
     while_loop()
